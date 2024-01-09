c26a800b6e908b4a07b93fded67ef7d0c2e81180
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2024-01-10 00:11:02 +0900

    중앙값 구하기 수정

--- 중앙값 구하기.js
+++ 중앙값 구하기.js
@@ -1,6 +1,6 @@
 function solution(array) {
   array.sort((a, b) => {
-    return a - b;
+    return a - b; // 숫자 오름차순으로 정렬하는 법
   });
   let answer = array[Math.floor(array.length / 2)];
   return answer;
