7aebcd476d394743656000628e2e25b37e922aea
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2024-01-16 21:00:40 +0900

    개미 군단

---
+++ 개미 군단.js
@@ -0,0 +1,6 @@
+function solution(hp) {
+  let general = Math.floor(hp / 5);
+  let soldier = Math.floor((hp - general * 5) / 3);
+  let worker = Math.floor(hp - (general * 5 + soldier * 3));
+  return general + soldier + worker;
+}
