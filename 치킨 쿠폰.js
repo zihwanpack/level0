a4ded4a20c7fa203cf4559edd2059d86004820e4
Layton <jihwan8807@naver.com>
2024-03-26 22:55:13 +0900

    치킨 쿠폰

---
+++ 치킨 쿠폰.js
@@ -0,0 +1,9 @@
+function solution(chicken) {
+  let answer = 0;
+
+  while (chicken >= 10) {
+    answer += Math.floor(chicken / 10);
+    chicken = Math.floor(chicken / 10) + Math.floor(chicken % 10);
+  }
+  return answer;
+}
